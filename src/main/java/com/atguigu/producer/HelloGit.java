c646a101f4d59b02f2a2d6f6bdda68181c6bca30
lds <lds691228@163.com>
2022-03-05 22:29:38 +0800

    hotfix first commit

--- src/main/java/com/atguigu/producer/HelloGit.java
+++ src/main/java/com/atguigu/producer/HelloGit.java
@@ -7,5 +7,6 @@ package com.atguigu.producer;
 public class HelloGit {
     public static void main(String[] args) {
         System.out.println("hello git3");
+        System.out.println("hello hot-fix");
     }
 }
