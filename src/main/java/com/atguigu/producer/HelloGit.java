8554311e78ae189b7096f775223bfeb216806abd
lds <lds691228@163.com>
2022-03-06 12:04:34 +0800

    3.7 first commit

--- src/main/java/com/atguigu/producer/HelloGit.java
+++ src/main/java/com/atguigu/producer/HelloGit.java
@@ -8,5 +8,6 @@ public class HelloGit {
     public static void main(String[] args) {
         System.out.println("hello git3");
         System.out.println("hello hot-fix");
+        System.out.println("12:00");
     }
 }
